d08a17d4e5bddd816ae88cd7f0a2e8106ed251b7
pramod <sablepramod01@gmail.com>
2018-10-04 22:19:37 +0530

    Images added

--- src/main/java/com/brocker/controller/HomeController.java
+++ src/main/java/com/brocker/controller/HomeController.java
@@ -6,11 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	@GetMapping("/")
+	@GetMapping("/login")
 	public String login(Model model) {
 
 		model.addAttribute("name", "John Doe");
 		System.out.println("LOGIN INITIATED");
 		return "login";
 	}
+
+	@GetMapping("/")
+	public String home(Model model) {
+
+		model.addAttribute("name", "John Doe");
+		System.out.println("LOGIN INITIATED");
+		return "home1";
+	}
+
 }
